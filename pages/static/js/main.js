351203e7272f532fe4278725493924919667bfa1
Yefri Ramos <yeframos@gmail.com>
2021-07-10 11:03:47 -0600

    Creacion de las paginas de: Login, Register y index
    Se añaden las primeras vistas de inicio de sesion y registro

---
+++ pages/static/js/main.js
@@ -0,0 +1,115 @@
+$(function() {
+	/* Sign Up Form */
+    $("#registerForm").validator().on("submit", function(event) {
+    	if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            sformError();
+            ssubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            ssubmitForm();
+        }
+    });
+
+    function ssubmitForm() {
+        // initiate variables with form content
+		var firstname = $("#rfirstname").val();
+		var lastname = $("#rlastname").val();
+		var email = $("#remail").val();
+		var password = $("#rpassword").val();
+        var passwordr = $("#rpasswordr").val();
+        
+        //peticion que espera una variable text
+        $.ajax({
+            type: "POST",
+            url: "",
+            data: "firstname=" + firstname + "&lastname=" + lastname + "email=" + email + "&password=" + password + "&passwordr=" + passwordr, 
+            success: function(text) {
+                if (text == "success") {
+                    sformSuccess();
+                } else {
+                    sformError();
+                    ssubmitMSG(false, text);
+                }
+            }
+        });
+	}
+
+    function sformSuccess() {
+        $("#signUpForm")[0].reset();
+        ssubmitMSG(true, "Sign Up Submitted!");
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+    }
+
+    function sformError() {
+        $("#signUpForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass();
+        });
+	}
+
+    function ssubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#smsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+
+
+    /* Log In Form */
+    $("#loginForm").validator().on("submit", function(event) {
+    	if (event.isDefaultPrevented()) {
+            // handle the invalid form...
+            lformError();
+            lsubmitMSG(false, "Please fill all fields!");
+        } else {
+            // everything looks good!
+            event.preventDefault();
+            lsubmitForm();
+        }
+    });
+
+    function lsubmitForm() {
+        // initiate variables with form content
+		var email = $("#lemail").val();
+		var password = $("#lpassword").val();
+		var remember = $("#customCheck").val();
+        
+        $.ajax({
+            type: "POST",
+            url: "",
+            data: "email=" + email + "&password=" + password + "&remember=" + remember, 
+            success: function(text) {
+                if (text == "success") {
+                    lformSuccess();
+                } else {
+                    lformError();
+                    lsubmitMSG(false, text);
+                }
+            }
+        });
+	}
+
+    function lformSuccess() {
+        $("#logInForm")[0].reset();
+        lsubmitMSG(true, "Log In Submitted!");
+        $("input").removeClass('notEmpty'); // resets the field label after submission
+    }
+
+    function lformError() {
+        $("#logInForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass();
+        });
+	}
+
+    function lsubmitMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center tada animated";
+        } else {
+            var msgClasses = "h3 text-center";
+        }
+        $("#lmsgSubmit").removeClass().addClass(msgClasses).text(msg);
+    }
+});
