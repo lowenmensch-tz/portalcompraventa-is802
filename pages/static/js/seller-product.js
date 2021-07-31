a2ac56952eb64e35aead046454c410c419302b4d
lowenmensch-tz <kenneth-cruz@outlook.com>
2021-07-30 22:22:23 -0700

    Pantalla modal para modificar un artículo

--- pages/static/js/seller-product.js
+++ pages/static/js/seller-product.js
@@ -146,11 +146,18 @@ function loadProducts(products, visible){
                     `;
 
                     //Delete product
+                    // data-target="#exampleModal"
+                    //         data-toggle="modal"
                     if (visible) {
                         document.getElementById(`deleteProduct-${index}`).innerHTML = `
                             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
                             width="24" height="24"
                             viewBox="0 0 172 172"
+
+                            class="close"
+                            type="button"
+                            onclick="deleteProduct(${products[index][0]}, ${products[index][1]})"
+                            
                             style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#666666"><path d="M33.73372,23.59961l-10.13411,10.13411l52.26628,52.26628l-52.26628,52.26628l10.13411,10.13411l52.26628,-52.26628l52.26628,52.26628l10.13411,-10.13411l-52.26628,-52.26628l52.26628,-52.26628l-10.13411,-10.13411l-52.26628,52.26628z"></path></g></g></svg>
                         `;
 
@@ -252,5 +259,39 @@ function ratingAndComment(){
         }
     }
     });
+
+}
+
+
+function deleteProduct(id, name){
+
+    document.body.innerHTML +=`
+    `;
+
+    document.getElementById("exampleModal").innerHTML = `
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Advertencia</h5>
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ¿Desea eliminar <strong>${name}</strong> de su lista de productos?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <button type="button" class="btn btn-primary">Aceptar</button>
+            </div>
+            </div>
+        </div>
+        
+    </div>    
+    `;
 
 }
