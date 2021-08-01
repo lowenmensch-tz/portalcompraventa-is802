0ca5e6785bcc49e51662647b0b056a4bac025362
Lxr-Dev <alexis_danielGT@hotmail.com>
2021-07-31 22:43:49 -0600

    Se agregó la funcionalidad del carrusel de imágenes

--- pages/static/js/marazo-js/detail.js
+++ pages/static/js/marazo-js/detail.js
@@ -1,24 +1,27 @@
+thumbStep = -285;
+thumbTotalPages = 0;
+
 window.onload = function(){
     sessionCheck();
     loadData();
-    // loadComments();
-    // showComments();
 };
 
 var comentarios;
 var count = 1;
 var commentPublisher;
 
-
+/**
+ * Verifica si el usuario está logeado
+ */
 function sessionCheck(){
-    var response;
+    var response; //Variable que guarda la respuesta del servidor
 
+    //Petición ajax para obtener las validaciones del usuario
     $.ajax({
         type: "GET",
         url: "ajax/loggedValidator",
         success: function(data) {
             response = data.logged;
-            console.log(response);
 
             if (response == 'true'){
                 $('#contact-review-forms').show();
@@ -37,9 +40,11 @@ function sessionCheck(){
     });
 }
 
+/**
+ * Carga todos los datos del producto, vendedor, Imágenes y Comentarios
+ */
 function loadData(){
 
-    // Esta petición es del tipo GET ya que se pasan los parámetros por URL para saber la información del Artículo y del Vendedor
     rate = 0;
 
     $.ajax({
@@ -51,7 +56,6 @@ function loadData(){
         success: function(data) {
         
             let url = "seller/" + convertURL(data.name, data.idPublisher); 
-            console.log(data)
             commentPublisher = data.publisher[0];
 
             document.getElementById("url-seller").onclick = function(){
@@ -60,28 +64,82 @@ function loadData(){
 
             document.getElementById('productName').innerHTML = data.title; //Nombre del producto
 
-            console.log(data.rating)
             $('#productDetailRating').rateit({max: 5, step: 1, value : parseFloat(data.rating), resetable : false , readonly : true}); // Promedio de Valoraciones
         
             document.getElementById('productDescription').innerHTML = `<p>${data.description}</p>`; //Descripción del Producto
         
-            document.getElementById('productPrice').innerHTML = `HNL ${data.price}`;
-            document.getElementById('publisher').value = `${data.name}`;
-            document.getElementById('publisherPhone').value = `${data.phone}`;
-            document.getElementById('publisherEmail').value = `${data.email}`;
-            document.getElementById('publisherAddress').value = `${data.address}`;
+            document.getElementById('productPrice').innerHTML = `HNL ${data.price}`; //Precio del producto
+            document.getElementById('publisher').value = `${data.name}`; //Vendedor
+            document.getElementById('publisherPhone').value = `${data.phone}`;  //Teléfono del vendedor
+            document.getElementById('publisherEmail').value = `${data.email}`; // Correo del vendedor
+            document.getElementById('publisherAddress').value = `${data.address}`; // Dirección del vendedor
 
+            // Cargar Imágenes
+            cuadroImagenes = '';
+            thumbImagenes = '';
 
-            document.getElementById('owl-single-product').innerHTML = '';
-            document.getElementById('owl-single-product-thumbnails').innerHTML = '';
+            cuadroImagenes =`<div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 7020px; left: 0px; display: block; transform: translate3d(0px, 0px, 0px);">`;
+            thumbImagenes = `<div class="owl-wrapper-outer"><div id="thumb-translate" class="owl-wrapper" style="width: 1764px; left: 0px; display: block; transform: translate3d(0px, 0px, 0px); transition: all 200ms ease 0s;">`;
+
+            // Convertir el JSON de imágenes en una lista
+            imagenes = []
+            for (image in data.image){
+                imagenes.push(data.image[image])
+            }
+
+            // Insertar cada imágen y thumbnails
+            for (i = 0; i< imagenes.length; i++){
+                cuadroImagenes += `
+                    <div class="owl-item" style="width: 390px;">
+                        <div class="single-product-gallery-item" id="slide${i+1}">
+                            <a data-lightbox="image-1" data-title="Galeria" href="${imagenes[i]}">
+                                <img class="img-responsive" alt="" src="https://drive.google.com/uc?export=view&id=1kWdiW6kBydwbjkmhzdLtcfq0YsbvmmWT" data-echo="${imagenes[i]}">
+                            </a>
+                        </div>
+                    </div>
+                `;
+                if (i==0){
+                    thumbImagenes +=`
+                        <div class="owl-item" style="width: 98px;">
+                            <div class="item">
+                                <a class="horizontal-thumb active" data-target="#owl-single-product" data-slide="${i+1}" href="#slide${i+1}">
+                                    <img class="img-responsive" alt="" src="https://drive.google.com/uc?export=view&id=1kWdiW6kBydwbjkmhzdLtcfq0YsbvmmWT" data-echo="${imagenes[i]}" />
+                                </a>
+                            </div>
+                        </div>
+                    `;
+                }else{
+                    thumbImagenes +=`
+                        <div class="owl-item" style="width: 98px;">
+                            <div class="item">
+                                <a class="horizontal-thumb" data-target="#owl-single-product" data-slide="${i+1}" href="#slide${i+1}">
+                                    <img class="img-responsive" alt="" src="https://drive.google.com/uc?export=view&id=1kWdiW6kBydwbjkmhzdLtcfq0YsbvmmWT" data-echo="${imagenes[i]}"/>
+                                </a>
+                            </div>
+                        </div>
+                    `;
+                }
+                
+            }
+
+            cuadroImagenes +=`</div></div>`;
+            thumbImagenes +=`</div></div><div class="owl-controls clickable"><div class="owl-pagination"><div id="thumbPage0" data-page="0" onclick="moveThumb(this);" class="owl-page active"><span class=""></span></div>`;
             
-            document.getElementById('owl-single-product').innerHTML += `
-                <div class="single-product-gallery-item" id="slide1">
-                    <a data-lightbox="image-1" data-title="Galeria" href="${data.image.photo0}">
-                        <img class="img-responsive" alt="" src="https://drive.google.com/uc?export=view&id=1kWdiW6kBydwbjkmhzdLtcfq0YsbvmmWT" data-echo="${data.image.photo0}">
-                    </a>
-                </div>
-            `;
+            // Paginador del Carrusel
+            thumbPages = Math.ceil(imagenes.length/3);
+            thumbTotalPages = thumbPages;
+
+            for (i=1; i<thumbPages;i++){
+                thumbImagenes += `
+                    <div id="thumbPage${i}" data-page="${i}" onclick="moveThumb(this);" class="owl-page"><span class=""></span></div>
+                `;
+            }
+            thumbImagenes += `</div></div>`;
+
+            document.getElementById('owl-single-product').innerHTML = cuadroImagenes;
+            document.getElementById('owl-single-product-thumbnails').innerHTML = thumbImagenes;
+
+            // Favoritos
             document.getElementById('addToFavorite').innerHTML +=`
             <div  class="favorite-button m-t-5">
                 <a onclick="addWish(${data.id_articulo});" class="btn btn-primary" data-toggle="tooltip" data-placement="right" title="Agregar a Favoritos" href="#">
@@ -130,8 +188,23 @@ function loadData(){
     });
 }
 
+/**
+ * Actualiza los estilos del paginador y mueve el carrusel de imágenes
+ * @param {Object} e Objeto que contiene la página seleccionada
+ */
+function moveThumb(e){
+    document.getElementById('thumb-translate').style = `width: 1764px; left: 0px; display: block; transform: translate3d(${parseInt(e.dataset.page)*thumbStep}px, 0px, 0px); transition: all 200ms ease 0s;`;
+    for(i=0;i<thumbTotalPages;i++){
+        document.getElementById(`thumbPage${i}`).className = "owl-page";
+    }
+    e.className = "owl-page active";
+}
+
+/**
+ * Actualiza los comentarios que se han ingresado
+ */
 function updateReviews(){
-    // Calcular las estrellas
+
     $.ajax({
         type: "POST",
         url:  `ajax/review`,
@@ -148,7 +221,6 @@ function updateReviews(){
               }
               catch(err) {}
             document.getElementById('pincheMarazo').parentNode.removeChild(document.getElementById('pincheMarazo'));
-            
 
             document.getElementById('commentsRow').innerHTML += `
             <div class="col-md-2 col-sm-2">
@@ -168,29 +240,23 @@ function updateReviews(){
         }
     });
     
-    
-
-    // Aquí se envían los datos a la base de datos y este responde con el nuevo cálculo y un nuevo listado de comentarios que sea guardará en la variable comentarios
-
-    
 }
 
-function loadComments(){
-    // Trae todos los comentarios de este producto y los guarda en la variable comentarios
-    console.log('Lista con todos los comentarios')
-}
-
-function showComments(){
-    //Aqui se cargan en la vista los comentarios desde la variable comentarios
-}
-
-
+/**
+ * Utiliza el nombre y el id del producto para generar una url única y dinámica
+ * @param {String} nameProduct Nombre del producto
+ * @param {Integer} idProduct Id del producto
+ * @returns 
+ */
 function convertURL(nameProduct, idProduct){
     return  idProduct.toString() + '-' + nameProduct.toLowerCase().replaceAll(/ /g,'-').replaceAll(/[^\w-]+/g,'');
 }
 
 
-
+/**
+ * Agrega a la lista de favoritos un nuevo artículo
+ * @param {Integer} id_articulo Id del articulo
+ */
 function addWish(id_articulo){
     $.ajax({
         type: "POST",
